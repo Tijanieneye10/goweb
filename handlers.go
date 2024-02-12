703cad222590baac62e7d3906cca2b6172732157
Tijani Eneye <brainyworld10@gmail.com>
2024-02-12 20:04:21 +0100

    template rendering

--- handlers.go
+++ handlers.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"github.come/Tijanieneye10/goweb/pkg/config"
 	"html/template"
 	"log"
 	"net/http"
@@ -13,20 +13,25 @@ func home(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "home.page.gohtml")
 }
 
+var app *config.AppConfig
+
+// Create a new template
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
 func renderTemplate(w http.ResponseWriter, gohtml string) {
-	tc, err := createTemplateCache()
-	if err != nil {
-		log.Fatalln(err)
-	}
+
+	tc := app.TemplateCache
 
 	t, ok := tc[gohtml]
 
 	if !ok {
-		log.Fatalln(err)
+		log.Fatalln("could not get template from template cache")
 	}
 
 	buf := new(bytes.Buffer)
-	err = t.Execute(buf, nil)
+	err := t.Execute(buf, nil)
 
 	if err != nil {
 		log.Println(err)
@@ -37,13 +42,13 @@ func renderTemplate(w http.ResponseWriter, gohtml string) {
 		log.Println(err)
 	}
 
-	parsedTemplate, _ := template.ParseFiles("./templates/"+gohtml, "./templates/base.layout.gohtml")
-
-	err = parsedTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("error parsing template", err)
-	}
+	//parsedTemplate, _ := template.ParseFiles("./templates/"+gohtml, "./templates/base.layout.gohtml")
+	//
+	//err = parsedTemplate.Execute(w, nil)
+	//
+	//if err != nil {
+	//	fmt.Println("error parsing template", err)
+	//}
 }
 
 func createTemplateCache() (map[string]*template.Template, error) {
--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"github.come/Tijanieneye10/goweb/pkg/config"
 	"net/http"
 )
 
 func main() {
+	var app config.AppConfig
+	tc, err := createTemplateCache()
+	if err != nil {
+		return
+	}
+
+	app.TemplateCache = tc
+
+	NewTemplates(&app)
+
 	http.HandleFunc("/", home)
 
-	err := http.ListenAndServe(":8000", nil)
+	err = http.ListenAndServe(":8000", nil)
 	if err != nil {
 		return
 	}
